7a90493eb1533823b3d5e72d141ba229748da23e
Andreas Åkerlöf <andreas@akerlof.se>
2018-11-07 14:25:28 +0100

    Övningar 2, uppdaterat

--- Övningar-2/script.js
+++ Övningar-2/script.js
@@ -49,3 +49,38 @@ if (veckoDag === 5)
     console.log('Du ska äta ute idag!');
 else 
     console.log('Dagens maträtt är: '+favoMat[Math.floor(Math.random()*favoMat.length)]);
+
+console.log('----');
+
+/*
+    Miljonären
+    Första dagen tjänar man 500kr och sedan får man 10% höjning varje månad.
+    Skriv ett program som räknar ut hur länge man behöver arbeta för att tjäna ihop minst en miljon.
+*/
+let salary = 500;
+let totalSalary = 0;
+let days = 1;
+while (totalSalary < 1000000) {
+    if (days % 30 === 0)
+        salary *= 1.10;
+    totalSalary += salary;
+    days++;
+}
+console.log('När du arbetat '+days+' dagar, har du tjänat ihop '+Math.round(totalSalary)+'kr.');
+
+console.log('---');
+
+// Mina får
+/* Mina får förökar sig snabbt
+och jag behöver ett
+program som kan räkna ut
+hur många de kommer att
+vara inom ett år.
+• Varje månad kommer fåren
+att multipliceras med 4. */
+
+let far = 2;
+for (let i = 0; i<12; i++) {
+    far *= 4;
+}
+console.log('Efter ett år har du '+far+' får.');
